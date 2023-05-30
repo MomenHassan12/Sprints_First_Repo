ac0fcbf12ed620f1d13b0bb0f977de46a21709de
momen <momenhassan@gmail.com>
2023-05-30 10:34:05 +0200

    update

--- S_DYNAMIC_10 Design a Small OS/SOS.h
+++ S_DYNAMIC_10 Design a Small OS/SOS.h
@@ -10,7 +10,6 @@
 #define SOS_H_
 
 
-#include <stdint.h>
 
 // Enumeration for system status
 typedef enum {
@@ -20,19 +19,18 @@ typedef enum {
 
 // Task structure
 typedef struct {
-    uint8_t taskID;
-    uint32_t intervalMs;          // Task execution interval in milliseconds
-    uint32_t lastExecutionTime;   // Timestamp of the last task execution
-    uint32_t executionTimeMs;     // Execution time of the task in milliseconds
-    void (*taskFunction)(void);
+    uint8_t u8_taskID;
+   uint8_t  u8_priority;              // Task priority to every task
+    uint32_t u32_intervalMs;          // Task execution interval in milliseconds
+    uint32_t u32_executionTimeMs;     // Execution time of the task in milliseconds
 } sos_task_t;
 
 // Function prototypes
 enu_system_status_t sos_init(void);
 enu_system_status_t sos_deinit(void);
-enu_system_status_t sos_create_task(uint32_t intervalMs, void (*taskFunction)(void), uint8_t* taskID);
-enu_system_status_t sos_delete_task(uint8_t taskID);
-enu_system_status_t sos_modify_task(uint8_t taskID, uint32_t intervalMs, void (*taskFunction)(void));
+enu_system_status_t sos_create_task(sos_task_t * ptr_sos_task);
+enu_system_status_t sos_delete_task(uint8_t u8_task_ID);
+enu_system_status_t sos_modify_task(uint8_t u8_task_ID,sos_task_t * ptr_sos_task);
 void sos_disable(void);
 void sos_run(void);
 
